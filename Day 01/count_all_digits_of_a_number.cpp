ad22955cc7dbd80055218492fcd569fe1197d39f
TarequeSyed <tareque1234syed@gmail.com>
2026-04-01 17:05:54 +0530

    Did 2 questions on basic math

---
+++ Day 01/count_all_digits_of_a_number.cpp
@@ -0,0 +1,29 @@
+// Example 1:
+// Input:N = 12345
+// Output:5
+// Explanation:  The number 12345 has 5 digits.
+
+/*Space complexity = O(logN)
+  Time complexity = O(1)*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        int count_digits(int n) {
+        int count = 0;
+        while(n != 0) {
+            n = n / 10;
+            count++;
+        }
+        return count;
+    }
+};
+
+int main() {
+    Solution obj;
+    int n = 159;
+    int result = obj.count_digits(n);
+    cout << "No. of digits in " << n << " is: " << result << endl;
+    return 0;
+}
---
+++ Day 01/reverse_a_number.cpp
@@ -0,0 +1,30 @@
+// Input: N = 12345
+// Output:54321
+// Explanation: The reverse of 12345 is 54321.
+
+/*Space complexity = O(logN)
+  Time complexity = O(1)*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+    int reverse_num(int n) {
+        int reversed = 0;
+        while (n != 0) {
+            int remainder = n % 10;
+            reversed = reversed * 10 + remainder;
+            n = n / 10;
+        }
+        return reversed;
+    }
+};
+
+int main() {
+    Solution ob;
+    int n = 53941;
+    int result = ob.reverse_num(n);
+    cout << "Original no.: " << n << endl;
+    cout << "Reversed number: " << result << endl;
+    return 0; 
+}
