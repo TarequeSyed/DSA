7c52895b2deba84bbdf5b56d5f14c3748f223e98
RizzWan-Codes <rizwanboss84@gmail.com>
2026-04-17 22:00:04 +0530

    solved 2 questions on arrays

--- Day 03 - Arrays/2nd_smallest_and_largest_element.cpp
+++ Day 03 - Arrays/2nd_smallest_and_largest_element.cpp
@@ -5,41 +5,36 @@ using namespace std;
     Space Complexity: O(1) */
 
 int second_max (vector<int>& arr) {
-    int max = arr[0], n = arr.size(), sec_max = INT_MAX;
+    int max = INT_MIN, n = arr.size(), sec_max = INT_MIN;
     // edge case: 
     if (n == 0 || n == 1) return -1;
-    for (int i = 1; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         if (arr[i] > max) {
+            sec_max = max;
             max = arr[i];
         }
-    }
-    for (int i = 1; i < n; i++) {
-        if (arr[i] == max) continue;
-        if (arr[i] > sec_max) {
+        else if (arr[i] > sec_max && arr[i] != max) {
             sec_max = arr[i];
-        }
+        } 
     }
     return sec_max;
 }
 
 int second_min (vector<int>& arr) {
-    int min = arr[0], sec_min, n = arr.size();
+    int min = INT_MAX, sec_min = INT_MAX, n = arr.size();
     // edge case: 
     if (n == 0 || n == 1) return -1;
-    for (int i = 1; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         if (arr[i] < min) {
+            sec_min = min;
             min = arr[i];
-        }
-    }
-
-    for (int i = 1; i < n; i++) {
-        if (arr[i] == min) continue;
-        if (arr[i] < sec_min) {
+        }    
+        else if (arr[i] < sec_min && arr[i] != sec_min) {
             sec_min = arr[i];
         }
     }
-    cout << sec_min << endl;
     return sec_min;
+    
 }
 
 int main() {
