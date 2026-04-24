073fa8bec01db93fd5ffb440de97dbabb462fd69
TarequeSyed <tareque1234syed@gmail.com>
2026-04-24 21:42:56 +0530

    Building intuition on how to shift the elements in an array

--- Day 07 - Arrays II/rotate_arr_by_k_places_to_do.cpp
+++ Day 07 - Arrays II/rotate_arr_by_k_places_to_do.cpp
@@ -7,19 +7,33 @@ Output : [3, 4, 5, 6, 1, 2] */
 #include <bits/stdc++.h>
 using namespace std;
 
+// Thinking of doing this using brute force first, then optimal method. I wanna try this myself.
 void rotate_k_place_by_right (vector<int>& arr, int k) {
     int n = arr.size() - 1;
-    int temp = arr[n];
-    for (int i = 0; i < n; i++) {
-        while(k--) {
-            
-        }    
+    vector<int> temp;
+
+    for (int i = arr[n]; i >= n-k; i--) {
+        temp.push_back(arr[i]);
     }
-    arr[0] = temp;
+    for (int i = 0; i < n-k; i++) {
+        temp.push_back(arr[i]);
+    }
+    for (int x : temp) {
+        cout << x << " ";
+    }
 }
 
 int main () {
-    
-
+    vector<int> arr = {2, 1, 3, 4, 5};
+    int k = 2;
+    cout << "Initial array was: " << endl;
+    for (int x : arr) {
+        cout << x << " ";
+    }
+    cout << endl << "After rotating by " << k << " places: " << endl;
+    rotate_k_place_by_right(arr, k);
+    for (int x : arr) {
+        cout << x << " ";
+    }
     return 0;
 }
