44c7f9bce50a6f9e3b016c1ea19183bb2e707132
TarequeSyed <tareque1234syed@gmail.com>
2026-04-25 23:17:56 +0530

    learned stocks_buy_and_sell brute force

--- Day 13_24_april - Binary Search + Array/stocks_buy_and_sell.cpp
+++ Day 13_24_april - Binary Search + Array/stocks_buy_and_sell.cpp
@@ -6,7 +6,29 @@ Output: 5
 Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 Note: That buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.*/
 
-//to do (to_do)
+// optimal to_do
+
 #include <bits/stdc++.h>
 using namespace std;
 
+// brute method: 
+/*Time: O(N^2),
+  Space: O(1)*/
+int max_profit (vector<int>& arr) {
+    int max_1 = 0;
+    for (int i = arr.size() - 1; i >= 0; i--) {
+        for (int j = 0; j < i; j++) {
+            
+            max_1 = max(arr[i]-arr[j], max_1);
+        }
+    }
+    return max_1;
+}
+
+int main() {
+    vector<int> arr = {7,1,5,3,6,4};
+    int res = max_profit(arr);
+
+    cout << "Max profit is: " << res << endl;
+    return 0;
+}
