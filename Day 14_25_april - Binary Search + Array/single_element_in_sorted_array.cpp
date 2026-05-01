947deff1909667f774752f197ed4ba9627252748
TarequeSyed <tareque1234syed@gmail.com>
2026-05-01 16:22:11 +0530

    Learned the optimal way to find single element in sorted arr

--- Day 14_25_april - Binary Search + Array/single_element_in_sorted_array.cpp
+++ Day 14_25_april - Binary Search + Array/single_element_in_sorted_array.cpp
@@ -9,7 +9,7 @@ Explanation: Only the number 4 appears once in the array.*/
 using namespace std;
 
 // Brute force: using XOR
-// Optimal approach: using Binary search; Time: O(logN), Space: O(1)  to_do or to do
+// Optimal approach: using Binary search; Time: O(logN), Space: O(1)
 
 // 1. Brute:
 int single_element_in_sorted_arr (vector<int>& arr) {
@@ -20,11 +20,32 @@ int single_element_in_sorted_arr (vector<int>& arr) {
     return ans;
 }
 
+// 2. Optimal: 
+int single_ele_in_sorted_arr_optimal (vector<int>& arr) {
+    int start = 0, end = arr.size() - 1;
+    // doing using parity tricks:
+    while (start < end) {
+        int mid = start + (end - start)/2;
+        if (mid % 2 == 1) mid--;
+
+        if (arr[mid] == arr[mid+1]) {
+            start = mid + 2;
+        }
+        else {
+            end = mid;
+        }
+    }
+    return arr[start];    
+}
 int main () {
     vector<int> arr = {1,1,2,2,3,3,4,5,5,6,6};
     int res = single_element_in_sorted_arr(arr);
 
     cout << "Single element in sorted array is: " << res << endl;
 
+
+    int resu = single_ele_in_sorted_arr_optimal(arr);
+    
+    cout << "Single element in sorted arr (optimal): " << resu << endl;
     return 0;
 }
