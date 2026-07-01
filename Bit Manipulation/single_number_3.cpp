f7ee0f127e325f4723d55866dc48bdab78386313
TarequeSyed <tareque1234syed@gmail.com>
2026-07-01 21:27:33 +0530

    learned intuition

--- Bit Manipulation/single_number_3.cpp
+++ Bit Manipulation/single_number_3.cpp
@@ -18,7 +18,7 @@ The integers 3 and 5 have appeared only once.
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> my_approach (vector<int>& arr) {
+vector<int> my_approach (vector<int> arr) {
     unordered_map <int, int> mp;
     vector<int> res;
     
@@ -34,6 +34,26 @@ vector<int> my_approach (vector<int>& arr) {
     return res;
 }
 
+vector<int> optimal (vector<int> arr) {
+    long XOR = 0;
+    for (int x : arr) {
+        XOR ^= x;
+    }
+    int rightmost = XOR & (-XOR);
+
+    int XOR1 = 0, XOR2 = 0;
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] & rightmost) {
+            XOR1 ^= arr[i];
+        }
+        else {
+            XOR2 ^= arr[i];
+        }
+    }
+    if (XOR1 < XOR2) return {XOR1, XOR2};
+    return {XOR2, XOR1};
+}
+
 int main() {
     vector<int> arr = {1, 2, 1, 3, 5, 2};
     vector<int> resu = my_approach(arr);
@@ -41,5 +61,14 @@ int main() {
     for (auto x : resu) {
         cout << x << " ";
     }
+    cout << endl;
+
+    vector<int> res = optimal(arr);
+    cout << "The answer with optimal: " << endl;
+
+    for (int y : res) {
+        cout << y << " ";
+    }
+
     return 0;
 }
